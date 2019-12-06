e9190b2a5c7e9cf2ffa09288cb2d127dd6644553
jimmykodes <james.keith.92@gmail.com>
2019-12-06 10:42:20 -0800

    made new zsh file specific to ssh shortcuts

--- aliases.zsh
+++ aliases.zsh
@@ -27,18 +27,3 @@ alias imagefirstshell='USE_POSTGRES=1 python manage.py shell_plus'
 # PyCharm Aliases
 #######################################
 alias rmpycache='find . -name *.pyc -delete && echo pycache files removed'
-
-# SSH aliases
-#######################################
-# raspberry pi
-alias piconnect='ssh pi@192.168.1.148'
-
-# V2 staging server connection
-alias v2_connect='ssh ubuntu@134.158.74.47'
-
-#mini ssh
-alias mini_ssh='ssh minikeith@keith-mini.local'
-
-#ubuntu machine
-alias ubuntu_connect='ssh jimmy@192.168.1.145'
-alias office_connect='ssh jimmy@205.173.78.34'
