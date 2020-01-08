7bee18a6573b56e92a008599fb681b1cc515b045
jimmykodes <james.keith.92@gmail.com>
2020-01-08 10:13:12 -0800

    added more docker aliases

--- aliases.zsh
+++ aliases.zsh
@@ -3,12 +3,15 @@
 alias ls='ls -GF'
 alias la='ls -AF'
 alias ll='ls -alFh'
+alias l='ls -alFh'
 
 # Docker Aliases
 #######################################
 alias dcdb='docker-compose exec django bash'
 alias dcd='docker-compose down'
 alias dcr='docker-compose restart'
+alias dcs='docker-compose stop'
+alias dst='docker status'
 alias dcud='docker-compose up -d'
 alias dcsp='docker-compose exec django python manage.py shell_plus'
 alias dcm='docker-compose exec django python manage.py migrate'
@@ -30,4 +33,4 @@ alias imagefirstshell='USE_POSTGRES=1 python manage.py shell_plus'
 
 # PyCharm Aliases
 #######################################
-alias rmpycache='find . -name *.pyc -delete && echo pycache files removed'
+alias rmpc='find . -name *.pyc -delete && echo pycache files removed'
