0dca787811bf2e7c75ac191990f3b4001ebfab98
jimmykodes <james.keith.92@gmail.com>
2020-10-25 14:48:24 -0700

    remove ssh.zsh

--- ssh.zsh
+++
@@ -1,8 +0,0 @@
-# raspberry pi
-alias piconnect='ssh pi@192.168.1.148'
-
-#mini ssh
-alias mini_ssh='ssh minikeith@keith-mini.local'
-
-#ubuntu machine
-alias ubuntu_connect='ssh jimmy@192.168.1.145'
