cc9e13ab1a50e580599bfa0a2bda4c6521ce922a
jimmykodes <james.keith.92@gmail.com>
2020-10-25 13:52:17 -0700

    remove ruby.zsh

--- ruby.zsh
+++
@@ -1,4 +0,0 @@
-export LDFLAGS="-L/usr/local/opt/ruby/lib"
-export CPPFLAGS="-I/usr/local/opt/ruby/include"
-export PKG_CONFIG_PATH="/usr/local/opt/ruby/lib/pkgconfig"
-export PATH="/usr/local/opt/ruby/bin:$PATH"
