81c29a6ecf042a3464e50d714ae45ecf09d9e796
jimmykodes <jkeith@kochava.com>
2021-06-07 14:12:12 -0700

    update pyenv init scripts

--- pyenv.zsh
+++ pyenv.zsh
@@ -1,4 +1,9 @@
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+if which pyenv > /dev/null; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init --path)"
+    eval "$(pyenv init -)";
+fi
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 alias mkvenv='pyenv virtualenv'
