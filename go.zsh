5a77d1a99eb48fb3b34074bc5101315f29f9dcf4
jimmykodes <jkeith@kochava.com>
2022-01-12 12:48:41 -0800

    add go mod init alias

--- go.zsh
+++ go.zsh
@@ -3,6 +3,7 @@ export PATH=$HOME/go/bin:$PATH
 export GOPATH=$HOME/go
 
 alias ggclip='go get $(pbpaste | sed "s/http[s]*:\/\///")'
+alias gmi='go mod init'
 alias gmv='go mod vendor'
 alias gmt='go mod tidy'
 alias gorm='go run main.go'
