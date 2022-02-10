9967a9550d2999d8f008654801bfc8e418b5fcc4
jimmykodes <jkeith@kochava.com>
2022-02-10 12:50:35 -0800

    fix: rename minikube commands

--- minikube.zsh
+++ minikube.zsh
@@ -1,20 +1,21 @@
-alias mkba='kubectl --context minikube apply'
-alias mkbak='kubectl --context minikube apply -k'
-alias mkbd='kubectl --context minikube delete'
-alias mkbdk='kubectl --context minikube delete -k'
+alias mkb='kubectl --context minikube'
+alias mkba='mkb apply'
+alias mkbak='mkba -k'
+alias mkbd='mkb delete'
+alias mkbdk='mkbd -k'
 
-mkp() {
-    image=$(basename $PWD)
-    docker push "$(minikube ip):5000/${1:-$image}:local"
-}
-
-mkb() {
+mkbb() {
     image=$(basename $PWD)
     docker build -t $(minikube ip):5000/${1:-$image}:local .
 }
 
 mkbp() {
-    mkb "$@"
-    mkp "$@"
+    image=$(basename $PWD)
+    docker push "$(minikube ip):5000/${1:-$image}:local"
+}
+
+mkbbp() {
+    mkbb "$@"
+    mkbp "$@"
 }
 
