ba48e0a20557ea77d0aafb6a57dba369047fc59a
jimmykodes <jimmy@khanacademy.org>
2022-11-22 13:24:42 -0800

    feat: alias python3

--- python.zsh
+++ python.zsh
@@ -2,9 +2,9 @@ alias pip-uninstall-all='pip freeze | xargs pip uninstall -y'
 alias rmpc='find . -name *.pyc -delete && echo pycache files removed'
 
 if [[ -d "/opt/homebrew" ]]; then
-  alias brewthon="/opt/homebrew/bin/python3"
+  alias python="/opt/homebrew/bin/python3"
 else
-  alias brewthon="/usr/local/bin/python3"
+  alias python="/usr/local/bin/python3"
 fi
 
 if [[ -f "/usr/local/bin/python2" ]]; then
