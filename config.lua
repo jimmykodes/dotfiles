fd8265a45dae309858a0b0c67deeaa313ba03461
jimmykodes <jimmy@khanacademy.org>
2023-02-15 12:30:55 -0800

    feat: add shellcheck and better tf volumes

--- config.lua
+++ config.lua
@@ -18,17 +18,18 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "yaml",
-  "go",
+    "bash",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "typescript",
+    "tsx",
+    "css",
+    "rust",
+    "yaml",
+    "go",
+    "hcl",
 }
 
 lvim.builtin.treesitter.highlight.enabled = true
@@ -37,28 +38,34 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 -- make sure server will always be installed even if the server is in skipped_servers list
 lvim.lsp.installer.setup.ensure_installed = {
-  "jsonls",
-  "gopls",
+    "jsonls",
+    "gopls",
 }
 
 -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  { command = "black", filetypes = { "python" } },
-  { command = "goimports", filetypes = { "go" } },
+    { command = "black",    filetypes = { "python" } },
+    { command = "goimports" },
 }
 
 -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  { command = "flake8", filetypes = { "python" } },
+    { command = "flake8",     filetypes = { "python" } },
+    { command = "shellcheck", filetypes = { "zsh" } }
 }
 lvim.plugins = {
-  {
-    "ggandor/lightspeed.nvim",
-    event = "BufRead",
-  },
-  {
-    "terryma/vim-multiple-cursors",
-  },
+    {
+        "ggandor/lightspeed.nvim",
+        event = "BufRead",
+    },
+    {
+        "terryma/vim-multiple-cursors",
+    },
+    {
+        "ray-x/lsp_signature.nvim",
+        event = "BufRead",
+        config = function() require "lsp_signature".on_attach() end,
+    },
 }
--- tf.zsh
+++ tf.zsh
@@ -1,21 +1,56 @@
+#!/bin/bash
+
+project-root() {
+  local wd
+  local i=0
+  wd=$(pwd)
+  while true; do
+    i=$((i+1))
+    if [ $i -gt 15 ]; then
+      echo "max recursion depth reached"
+      return 1
+    fi
+    if stat -q "$wd"/.git > /dev/null; then
+      echo "$wd"
+      break
+    else
+      wd=$(dirname "$wd")
+    fi
+  done
+}
+
+relpath() {
+  local from=$1
+  local to=$2
+  local rel
+  while [ "$to" != "$from" ]; do
+    rel="$(basename "$from")/${rel}"
+    from=$(dirname "$from")
+  done
+  echo "$rel"
+}
+
 tf-version() {
   if [[ -e ".tf-version" ]]; then
     cat .tf-version
   elif [[ -e $HOME/.tf-version ]]; then
-    cat $HOME/.tf-version
+    cat "$HOME/.tf-version"
   else
     echo "latest"
   fi
 }
 
 tf() {
-
-  local cmd="docker run -it --rm -v $PWD:/app -v $HOME/.config/gcloud/:/root/.config/gcloud"
+  local projectRoot
+  local relativePath
+  projectRoot=$(project-root)
+  relativePath=$(relpath "$PWD" "$projectRoot")
+  local cmd="docker run -it --rm -v $projectRoot:/app -v $HOME/.config/gcloud/:/root/.config/gcloud"
   if [[ -d ../shared ]]; then
     cmd="${cmd} -v $PWD/../shared:/shared"
   fi
   if [[ -d ../shared-iam ]]; then
     cmd="${cmd} -v $PWD/../shared-iam:/shared-iam"
   fi
-  eval "${cmd} --platform linux/amd64 -w /app hashicorp/terraform:$(tf-version) $@"
+  eval "${cmd} --platform linux/amd64 -w /app/${relativePath} hashicorp/terraform:$(tf-version) $*"
 }
