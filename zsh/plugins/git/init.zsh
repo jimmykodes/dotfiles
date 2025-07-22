3e04582f4966b2b8c273bd7dc9ae4f969363c4d3
jimmykodes <jimmy@khanacademy.org>
2025-07-22 12:58:37 -0700

    feat: add wkcln to clone work dirs

--- zsh/plugins/git/init.zsh
+++ zsh/plugins/git/init.zsh
@@ -39,6 +39,16 @@ jkcln() {
 	git cl -r "jimmykodes/$repo" -d "$CODE_DIR/$repo" "$@"
 }
 
+wkcln() {
+	if [ -z "$WORK" ] || [ -z "$WORK_ORG" ]; then
+		echo "no work configs found. set WORK and WORK_ORG env vars."
+		return 1
+	fi
+	local repo="$1"
+	shift 1
+	git cl -r "$WORK_ORG/$repo" -d "$WORK/$repo" "$@"
+}
+
 gcb() {
 	local branch=$1
 	local issue=$2
