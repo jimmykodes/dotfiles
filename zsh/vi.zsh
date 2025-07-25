2fac9163ee2da299fda97112823a3a97b7862bf4
jimmykodes <jimmy@khanacademy.org>
2025-07-25 07:57:35 -0700

    fix: remove cursor blink

--- zsh/vi.zsh
+++ zsh/vi.zsh
@@ -1,33 +1,34 @@
 # Change cursor shape for different vi modes
+#  \e[0 q  - Default cursor (usually blinking block)
+#  \e[1 q  - Blinking block
+#  \e[2 q  - Steady block
+#  \e[3 q  - Blinking underline
+#  \e[4 q  - Steady underline
+#  \e[5 q  - Blinking bar/beam (I-beam)
+#  \e[6 q  - Steady bar/beam (I-beam)
 function zle-keymap-select {
 	if [[ ${KEYMAP} == vicmd ]] ||
 		[[ $1 = 'block' ]]; then
-		echo -ne '\e[1 q' # normal mode
+		echo -ne '\e[2 q' # normal mode
 	elif [[ ${KEYMAP} == main ]] ||
 		[[ ${KEYMAP} == viins ]] ||
 		[[ ${KEYMAP} == '' ]] ||
 		[[ $1 = 'beam' ]]; then
-		echo -ne '\e[5 q' # insert mode
+		echo -ne '\e[6 q' # insert mode
 	fi
 }
+
 zle -N zle-keymap-select
 
-# Set initial cursor to beam
-echo -ne '\e[5 q'
+# Set initial cursor
+echo -ne '\e[6 q'
 
 # Also handle when the shell starts
 function zle-line-init {
-	echo -ne '\e[5 q'
+	echo -ne '\e[6 q'
 }
-zle -N zle-line-init
 
-# •  \e[0 q  - Default cursor (usually blinking block)
-# •  \e[1 q  - Blinking block
-# •  \e[2 q  - Steady block
-# •  \e[3 q  - Blinking underline
-# •  \e[4 q  - Steady underline
-# •  \e[5 q  - Blinking bar/beam (I-beam)
-# •  \e[6 q  - Steady bar/beam (I-beam)
+zle -N zle-line-init
 
 autoload -z edit-command-line
 zle -N edit-command-line
