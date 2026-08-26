afe705d92f0f0cc6433cb9213ba69f78d71456ef
jimmykodes <james.keith.92@gmail.com>
2026-08-26 15:26:58 -0700

    fix: move wkcln out of dotfiles

--- zsh/plugins/git/init.zsh
+++ zsh/plugins/git/init.zsh
@@ -7,13 +7,3 @@ jkcln() {
 	shift 1
 	git cl -r "jimmykodes/$repo" -d "$CODE_DIR/$repo" "$@"
 }
-
-wkcln() {
-	if [ -z "$WORK" ] || [ -z "$WORK_ORG" ]; then
-		echo "no work configs found. set WORK and WORK_ORG env vars."
-		return 1
-	fi
-	local repo="$1"
-	shift 1
-	git cl -r "$WORK_ORG/$repo" -d "$WORK/$repo" "$@"
-}
